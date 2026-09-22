effd126bd2545340e1edcbfe022d0b23ffec3a7e
5000yearsag <ekeleo@gmail.com>
2026-09-22 14:27:22 +0545

    deploy preprod 2026-09-22T08:37:57Z

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,5 @@
-const releaseId = "20260922072138";
-const cacheName = "DefaultCompany-METAFAIR-0.1.0-20260922072138";
+const releaseId = "20260922082812";
+const cacheName = "DefaultCompany-METAFAIR-0.1.0-20260922082812";
 const contentToCache = [
   "Build/fair.loader.js",
   "Build/fair.framework.js.br",
