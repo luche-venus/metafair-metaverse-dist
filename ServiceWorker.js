eb74a290db293334c62613a09bbf63cc7bc23623
5000yearsag <ekeleo@gmail.com>
2026-09-25 14:14:20 +0545

    deploy: production Addressables 20260925082117

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,5 @@
-const releaseId = "20260925063006";
-const cacheName = "DefaultCompany-METAFAIR-0.1.0-20260925063006";
+const releaseId = "20260925082117";
+const cacheName = "DefaultCompany-METAFAIR-0.1.0-20260925082117";
 const contentToCache = [
   "Build/fair.loader.js",
   "Build/fair.framework.js.br",
